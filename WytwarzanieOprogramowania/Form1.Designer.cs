985f201e69a9cb1cf7b4783c5cd6af8ec16c162f
Patryk Szczodrowski <patrykszczodrowski1403@gmail.com>
2021-01-23 18:48:15 +0100

    Dodane rzutowanie na typy int,short,sbyte

--- WytwarzanieOprogramowania/Form1.Designer.cs
+++ WytwarzanieOprogramowania/Form1.Designer.cs
@@ -100,6 +100,7 @@ namespace Calculator
             // 
             // textBox2
             // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.textBox2.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.textBox2.Location = new System.Drawing.Point(2, 12);
             this.textBox2.Multiline = true;
